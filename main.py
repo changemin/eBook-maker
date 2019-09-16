99ef24f94f3a9d12658af46ae1d2d371df149a42
Changemin <bkm.change.min@gmail.com>
2019-09-16 21:29:59 +0900

    create main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from PIL import Image, ImageDraw, ImageFont
+import random
 
 WHITE = (255,255,255)
 
 for x in range(2):
-    filename = "poem-txt/poem("+str(x+1)+").txt"
+    fontPath = ""
+    poemPath = "poem-txt/poem("+str(x+1)+").txt"
     #print("read file : " + filename)
-    f = open(filename, "r", encoding='UTF8')
+    f = open(poemPath, "r", encoding='UTF8')
+
+    backgroundImgPath = "background-imgs/"+str(random.randrange(1,5))+".jpg"
+    backgroundImg = Image.open(backgroundImgPath)
 
     poemTitle = f.readline()
     poemAuthor = f.readline()
@@ -15,17 +20,17 @@ for x in range(2):
     print("시인:" + poemAuthor) #author of the poem
     #print(poemContent) #content
 
-    img = Image.new('RGB', (1000, 1000), color = (0,0,0,0))
+    #img = Image.new('RGB', (1000, 1000), color = (0,0,0,0))
 
     titleFont = ImageFont.truetype('font-files/KoPubDotumBold.ttf', 50)
     authorFont = ImageFont.truetype('font-files/KoPubDotumBold.ttf', 20)
     contentFont = ImageFont.truetype('font-files/KoPubDotumBold.ttf', 30)
 
-    d = ImageDraw.Draw(img)
+    d = ImageDraw.Draw(backgroundImg)
     d.text((100,100), poemTitle, font=titleFont, fill=WHITE)
     d.text((100,160), poemAuthor, font=authorFont, fill=WHITE)
     d.text((100,240), poemContent, font=contentFont, fill=WHITE)
 
     targetPath = "result/poemResult("+ str(x) +").png"
-    img.save(targetPath)
+    backgroundImg.save(targetPath)
     print("[LOG]Image Saved!")
