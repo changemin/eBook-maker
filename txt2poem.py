91359ec4435232559d7c4b0fee4b4a5c8ea5be0e
Changemin <bkm.change.min@gmail.com>
2019-09-16 12:01:29 +0900

    txt2poem.py update

--- txt2poem.py
+++ txt2poem.py
@@ -1,9 +1,12 @@
-#import string
-
 for x in range(1):
     filename = "poem-txt/poem("+str(x+1)+").txt"
-    print(filename)
+    print("read file : " + filename)
     f = open(filename, "r", encoding='UTF8')
-    print("시 제목 : " + f.readline()) #title of the poem
-    print("시인 : " + f.readline()) #author of the poem
-    print(f.read()) #content
+
+    poemTitle = f.readline()
+    poemAuthor = f.readline()
+    poemContent = f.read()
+
+    print("시 제목:"+poemTitle) #title of the poem
+    print("시인:"+poemAuthor) #author of the poem
+    print(poemContent) #content
